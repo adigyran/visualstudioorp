2328c042210574639b08823bb6620d55f87707d4
adigyran <adigyran@gmail.com>
2015-10-03 02:04:56 +0300

    ffd

--- master/master/master/Form1.Designer.cs
+++ master/master/master/Form1.Designer.cs
@@ -34,6 +34,11 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -78,6 +83,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 4;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
@@ -89,11 +95,54 @@
             this.label2.TabIndex = 5;
             this.label2.Text = "label2";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1068, 457);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 467);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(23, 518);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(489, 518);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1312, 687);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
@@ -116,6 +165,11 @@
         public System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- master/master/master/Form1.cs
+++ master/master/master/Form1.cs
@@ -6,10 +6,14 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Web;
 
 namespace master
 {
@@ -24,6 +28,7 @@ namespace master
         Process currentProc;
 
         BackgroundWorker bg;
+        BackgroundWorker bf;
 
         public static object ListBox1 { get; private set; }
 
@@ -43,6 +48,15 @@ namespace master
             bg.WorkerSupportsCancellation = true;
             bg.ProgressChanged += bg_ProgressChanged;
             button1.Enabled = false;
+
+            bf = new BackgroundWorker();
+            bf.DoWork += new DoWorkEventHandler(bf_DoWork);
+            bf.RunWorkerCompleted += bf_RunWorkerCompleted;
+            bf.WorkerReportsProgress = true;
+            bf.WorkerSupportsCancellation = true;
+            bf.ProgressChanged += bf_ProgressChanged;
+            button2.Enabled = false;
+
 
 
         }
@@ -81,7 +95,9 @@ namespace master
          void bg_DoWork(object sender, DoWorkEventArgs e)
         {
            // string dirPath = @"h:\downloads";
-             dirs = new List<string>(Directory.EnumerateDirectories(dirPath, "*", SearchOption.AllDirectories));
+             dirs = new List<string>();
+            dirs.Add(dirPath);
+            dirs.AddRange(Directory.EnumerateDirectories(dirPath, "*", SearchOption.AllDirectories));
             filesas = new List<FileInfo>();
             filesasstr = new List<string>();
             foreach (string dir in dirs)
@@ -137,6 +153,111 @@ namespace master
 
         }
 
+
+
+
+        void Writetofiledisk(string path, object Data)
+        {
+
+        }
+        void bf_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // string dirPath = @"h:\downloads";
+            // dirs = new List<string>(Directory.EnumerateDirectories(dirPath, "*", SearchOption.AllDirectories));
+            // filesas = new List<FileInfo>();
+            // filesasstr = new List<string>();
+            // foreach (string dir in dirs)
+            // {
+            //    DirectoryInfo di = new DirectoryInfo(dir);
+            //   // filesa = di.GetFiles("*", SearchOption.AllDirectories);
+            //  filesas.AddRange(di.GetFiles("*", SearchOption.TopDirectoryOnly));
+            Writetofiledisk("H:\text.txt", filesas);
+            bf.ReportProgress(filesas.Count);
+            int prgrsess = 0;
+            int remain = filesas.Count();
+
+
+            FileStream fs = new FileStream("DataFile.dat", FileMode.Create);
+
+            // Construct a BinaryFormatter and use it to serialize the data to the stream.
+            BinaryFormatter formatter = new BinaryFormatter();
+            try
+            {
+                foreach (FileInfo x in filesas)
+                {
+
+
+                    formatter.Serialize(fs, x);
+                    bf.ReportProgress(prgrsess);
+                    label3.Invoke(new Action(() => label3.Text = remain + " " + x.Name + " " + x.Length.ToString()));
+                    Thread.Sleep(1);
+                    prgrsess++;
+                    remain--;
+
+                }
+            }
+            catch (Exception exeption)
+            { MessageBox.Show(exeption.Message); }
+
+            finally
+            {
+                fs.Close();
+            }    
+            
+          
+
+            //StreamWriter file = new StreamWriter("test.txt",true);
+
+
+            //foreach (FileInfo x in filesas)
+            // {
+            //  file.WriteLine(x);
+            //  bf.ReportProgress(prgrsess);
+            //   Thread.Sleep(10);
+            //    prgrsess++;
+
+            //}
+
+            // file.Close();
+            //Thread.Sleep(1);
+
+
+        }
+
+        void bf_RunWorkerCompleted(object sender,
+                 RunWorkerCompletedEventArgs e)
+        {
+
+
+            //listBox1.Invoke(new Action(() => listBox1.DataSource = dirs));
+           // richTextBox1.Invoke(new Action(() => richTextBox1.Lines = filefot));
+           button2.Invoke(new Action(() => button2.Enabled = true));
+            //textBox1.Invoke(new Action(() => textBox1.Enabled = true));
+
+
+            // {
+            // label3.Content = "Random Cancelled " + Process.GetCurrentProcess().Threads.Count;
+            //}));
+            //listBox1.Invoke(       
+            // listBox1.DataSource = dirs;
+            //);
+        }
+        void bf_ProgressChanged(object sender,
+   ProgressChangedEventArgs e)
+        {
+
+            //long memory = (currentProc.PrivateMemorySize64/ 1024)/1024;
+            label1.Invoke(new Action(() => label1.Text = e.ProgressPercentage.ToString()));
+
+            //  Console.WriteLine("Обработано " + e.ProgressPercentage + "%");
+
+        }
+
+
+
+
+
+
         private void listBox1_SelectedValueChanged(object sender, EventArgs e)
         {
            string choiseddir =  listBox1.SelectedValue.ToString();
@@ -146,7 +267,8 @@ namespace master
             //FileInfo findtext = new FileInfo("");
            
             FileInfo[] files = filesas.FindAll(x => x.DirectoryName == choiseddir).ToArray();
-            
+           
+
             List<string> fileinfotext = new List<string>();
             long filesize = 0;
             long directorysize = 0;
@@ -158,20 +280,78 @@ namespace master
             }
             fileinfotext.Add(directorysize.ToString() + " Mbytes");
             richTextBox1.Lines = fileinfotext.ToArray();
+            button2.Enabled = true;
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             string txtbox = textBox1.Text;
             label2.Text = txtbox;
+            List < string > dirsen = new List<string>();
+            int directories = 0;
+           
             if(txtbox.Length>0 && Directory.Exists(txtbox))
             {
                 button1.Enabled = true;
-                
+                dirsen = Directory.EnumerateDirectories(txtbox).ToList();
+                label2.Text = label2.Text+" "+dirsen.Count.ToString();
+                //label2.Text = Directory.EnumerateFiles(txtbox).ToList().
+
             }
             else
             {
                 button1.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //StreamWriter file = new System.IO.StreamWriter(speichern);
+            //foreach (string x in ausgabeListe)
+            //    file.WriteLine(x);
+            //file.Close();
+            bf.RunWorkerAsync();
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bg.CancelAsync();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //string json = "{\"load\":\"load\"," +
+            //"\"n\":\"2\"}";
+
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://tickets.multifest.ru/php/base_connections.php");
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://url");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = new JavaScriptSerializer().Serialize(new
+                {
+                    user = "Foo",
+                    password = "Baz"
+                });
+
+                streamWriter.Write(json);
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
             }
         }
     }
